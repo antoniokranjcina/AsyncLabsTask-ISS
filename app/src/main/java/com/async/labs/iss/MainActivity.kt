3f740c140ad5eaf6ceba14a55e179ffa4c52b8cf
antoniokranjcina <54414989+antoniokranjcina@users.noreply.github.com>
2020-09-21 22:11:58 +0200

    Added navigation for new Fragments (HomeItemFragment)

--- app/src/main/java/com/async/labs/iss/MainActivity.kt
+++ app/src/main/java/com/async/labs/iss/MainActivity.kt
@@ -16,9 +16,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        window.statusBarColor = resources.getColor(R.color.theme_color, theme)
-        //window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-
         val navController = findNavController(R.id.fragment)
 
         binding.bottomNavigation.setupWithNavController(navController)
@@ -26,22 +23,45 @@ class MainActivity : AppCompatActivity() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.nav_twitter -> {
+                    setStatusBarAndToolbar(R.color.theme_color, true, "Twitter", true)
                     binding.bottomNavigation.visibility = View.VISIBLE
-                    setToolbar("Twitter")
                 }
                 R.id.nav_home -> {
+                    setStatusBarAndToolbar(R.color.theme_color, true, "Home", true)
                     binding.bottomNavigation.visibility = View.VISIBLE
-                    setToolbar("Home")
                 }
                 R.id.nav_location -> {
+                    setStatusBarAndToolbar(R.color.white, false, "", false)
                     binding.bottomNavigation.visibility = View.VISIBLE
-                    setToolbar("Location")
+                }
+                R.id.homeItemFragment -> {
+                    setStatusBarAndToolbar(R.color.theme_color, true, "", false)
+                    binding.bottomNavigation.visibility = View.GONE
                 }
             }
         }
     }
 
-    private fun setToolbar(title: String) {
-        binding.toolbar.toolbar_text_view_title.text = title
+    private fun setStatusBarAndToolbar(
+        color: Int,
+        whiteStatusBarLetters: Boolean,
+        toolbarTitle: String,
+        toolbar: Boolean
+    ) {
+        binding.toolbar.toolbar_text_view_title.text = toolbarTitle
+
+        window.statusBarColor = resources.getColor(color, theme)
+        if (!whiteStatusBarLetters) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE
+        }
+
+        if (toolbar) {
+            binding.toolbar.visibility = View.VISIBLE
+        } else {
+            binding.toolbar.visibility = View.GONE
+
+        }
     }
 }
